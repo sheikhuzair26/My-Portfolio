729ece3034e1e864dad11d33a5875f92a48e1132
Uzair Javed <uzairjaved75@gmail.com>
2019-09-06 17:58:46 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+(function($) {
+  var toggle = document.getElementById("menu-toggle");
+  var menu = document.getElementById("menu");
+  var close = document.getElementById("menu-close");
+
+  toggle.addEventListener("click", function(e) {
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+    } else {
+      menu.classList.add("open");
+    }
+  });
+
+  close.addEventListener("click", function(e) {
+    menu.classList.remove("open");
+  });
+
+  // Close menu after click on smaller screens
+  $(window).on("resize", function() {
+    if ($(window).width() < 846) {
+      $(".main-menu a").on("click", function() {
+        menu.classList.remove("open");
+      });
+    }
+  });
+
+})(jQuery);
